57d9929fe00dcfe049c3d2de776e2f924297a2f7
AjiNurAji <ajinuraji090306@gmail.com>
2023-12-22 17:31:52 +0700

    update preload

--- assets/js/preload.js
+++ assets/js/preload.js
@@ -1,10 +1,6 @@
 const preload = document.getElementById("preload");
 
 setTimeout(() => {
-  scrollTo({
-    top: 0,
-    smooth: true
-  })
   document.body.classList.remove("overflow-y-hidden")
   preload.classList.add("hidden");
 }, 1500);
